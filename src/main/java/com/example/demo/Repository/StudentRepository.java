f9642ac4341e44d0fa1dcba2b5482b14bb183e21
Min4Ml4a <6310210435@email.psu.ac.th>
2023-08-29 14:47:58 +0700

    sss

--- src/main/java/com/example/demo/Repository/StudentRepository.java
+++ src/main/java/com/example/demo/Repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Student;
 
 @Repository 
-public interface StudentRepository extends JpaRepository <student , integer>{
+public interface StudentRepository extends JpaRepository <Student , String>{
 	
 
 }
